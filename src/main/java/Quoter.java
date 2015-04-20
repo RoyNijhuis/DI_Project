0dae46dc74d9f1c74853a85aa19a34131ec3f384
RoyNijhuis <roynijhuis@hotmail.nl>
2015-04-20 15:50:20 +0200

    Initial commit

---
+++ src/main/java/Quoter.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Roy on 20-4-2015.
+ */
+public class Quoter {
+    public int getBookPrice(String s) {
+        switch(s) {
+            case "1":
+                return 10;
+            case "2":
+                return 45;
+            case "3":
+                return 20;
+            case "4":
+                return 35;
+            case "5":
+                return 50;
+            default:
+                return 0;
+        }
+    }
+}
---
+++ src/test/java/BookQuoteTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Roy on 20-4-2015.
+ */
+public class BookQuoteTest {
+
+}
